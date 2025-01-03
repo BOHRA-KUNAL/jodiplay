8633f367b41153640880279ebe0afedfbc241e42
Kunal Bohra <66618568+BOHRA-KUNAL@users.noreply.github.com>
2025-01-03 22:03:39 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import HomeMain from "@/components/HomeMain";
+import MovingText from "@/components/MovingText";
+import Head from "next/head"; // Import Head from next/head
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      {/* <Head>
+        <title>Jodi Play | The Best Online Satta Matka App for Fun and Wins</title> 
+      </Head> */}
+      {/* <MovingText /> */}
+      <HomeMain />
+    </div>
+  );
+}
+
+export default Home;
